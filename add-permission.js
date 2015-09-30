7c912e1b41edb21c3c197a867ad76ac8be8ee5b8
William Gaul <willyg302@gmail.com>
2015-09-29 21:53:41 -0700

    Fix indentation, use code block for command in readme

--- add-permission.js
+++ add-permission.js
@@ -1,47 +1,46 @@
 /**
-* A Lambda function that sets the permissions for the specified SNS topic
-**/
+ * A Lambda function that sets the permissions for the specified SNS topic
+ */
 var aws = require("aws-sdk");
- 
+
 exports.handler = function(event, context) {
- 
+
     console.log("REQUEST RECEIVED:\n" + JSON.stringify(event));
-    
+
     // For Delete requests, immediately send a SUCCESS response.
     if (event.RequestType == "Delete") {
         sendResponse(event, context, "SUCCESS");
         return;
     }
- 
+
     var responseStatus = "FAILED";
     var responseData = {};
- 
+
     var lambda = new aws.Lambda({region: event.ResourceProperties.Region});
     var addPermissionParams = {
-	Action: 'lambda:InvokeFunction', 
-  	FunctionName: event.ResourceProperties.LambdaFx, 
-  	Principal: 'sns.amazonaws.com', 
-  	StatementId: 'stmt-id-101',         
+        Action: 'lambda:InvokeFunction',
+        FunctionName: event.ResourceProperties.LambdaFx,
+        Principal: 'sns.amazonaws.com',
+        StatementId: 'stmt-id-101',
     };
- 
+
     // Get AMI IDs with the specified name pattern and owner
     lambda.addPermission(addPermissionParams, function(err, addPermissionResult) {
         if (err) {
             responseData = {Error: "Add Permission call failed"};
             console.log(responseData.Error + ":\n", err);
-        }
-        else {
-	    responseStatus = "SUCCESS";
-	    responseData = { "Result" : addPermissionResult };
+        } else {
+            responseStatus = "SUCCESS";
+            responseData = { "Result" : addPermissionResult };
         }
         sendResponse(event, context, responseStatus, responseData);
     });
 };
 
 
-// Send response to the pre-signed S3 URL 
+// Send response to the pre-signed S3 URL
 function sendResponse(event, context, responseStatus, responseData) {
- 
+
     var responseBody = JSON.stringify({
         Status: responseStatus,
         Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
@@ -51,12 +50,12 @@ function sendResponse(event, context, responseStatus, responseData) {
         LogicalResourceId: event.LogicalResourceId,
         Data: responseData
     });
- 
+
     console.log("RESPONSE BODY:\n", responseBody);
- 
+
     var https = require("https");
     var url = require("url");
- 
+
     var parsedUrl = url.parse(event.ResponseURL);
     var options = {
         hostname: parsedUrl.hostname,
@@ -68,22 +67,22 @@ function sendResponse(event, context, responseStatus, responseData) {
             "content-length": responseBody.length
         }
     };
- 
+
     console.log("SENDING RESPONSE...\n");
- 
+
     var request = https.request(options, function(response) {
         console.log("STATUS: " + response.statusCode);
         console.log("HEADERS: " + JSON.stringify(response.headers));
-        // Tell AWS Lambda that the function execution is done  
+        // Tell AWS Lambda that the function execution is done
         context.done();
     });
- 
+
     request.on("error", function(error) {
         console.log("sendResponse Error:" + error);
-        // Tell AWS Lambda that the function execution is done  
+        // Tell AWS Lambda that the function execution is done
         context.done();
     });
-  
+
     // write data to request body
     request.write(responseBody);
     request.end();
