eeec833f1ce9fa1cc696438422db6376a36c16ad
jrrudge <jrrudge@amazon.com>
2020-05-18 15:28:57 +0100

    updated test script

--- tests.sh
+++ tests.sh
@@ -1,42 +1,169 @@
 #!/bin/bash
 
+set -e
+
+if [ -z $1 ]; then
+    echo "Stack name not passed, exiting"
+    exit 1
+fi
+
+STACK=$1
+echo "$STACK"
+
+
+function clean {
+
+    ## Cleanup DB
+
+
+    for f in "${TEST_FILES[@]}"; do
+
+        FILE_NAME=$(cut -d "/" -f2- <<< "$f")
+
+        echo "Removing item from DB : $FILE_NAME"
+
+        aws dynamodb delete-item --table-name $DYNAMO_TABLE \
+        --key "{\"id\": {\"S\": \"s3://$BUCKET_IN/$FILE_NAME\"}}"
+
+
+        if [ $? -ne 0 ]; then
+            echo -e "Remove item failed" ; exit 1
+        fi
+
+    done
+
+    ## Cleanup Output
+
+
+    for f in "${TEST_FILES[@]}"; do
+
+      FILE_NAME=$(cut -d "/" -f2- <<< "$f")
+
+      echo "Removing from output : ${FILE_NAME/md/html}"
+
+      aws s3 rm s3://"$BUCKET_OUT"/"${FILE_NAME/md/html}"
+
+        if [[ $? -ne 0 ]]; then
+          echo -e "File not processed" ; exit 1
+        fi
+
+    done
+
+    ## Cleanup Input
+
+
+    for f in "${TEST_FILES[@]}"; do
+
+      FILE_NAME=$(cut -d "/" -f2- <<< "$f")
+
+      echo "Removing from input: $FILE_NAME"
+
+      aws s3 rm s3://"$BUCKET_IN"/"$FILE_NAME"
+
+        if [[ $? -ne 0 ]]; then
+          echo -e "File not processed" ; exit 1
+        fi
+
+
+    done
+
+
+}
+
 
 ## Get Stack Resources
 
-BUCKET_IN=$(aws cloudformation describe-stack-resource --stack-name lambda-file-refarch --logical-resource-id 'InputBucket' --query "StackResourceDetail.PhysicalResourceId" --output text)
-BUCKET_OUT=$(aws cloudformation describe-stack-resource --stack-name lambda-file-refarch --logical-resource-id 'ConversionTargetBucket' --query "StackResourceDetail.PhysicalResourceId" --output text)
-DYNAMO_TABLE=$(aws cloudformation describe-stack-resource --stack-name lambda-file-refarch --logical-resource-id 'SentimentTable' --query "StackResourceDetail.PhysicalResourceId" --output text)
+BUCKET_IN=$(aws cloudformation describe-stack-resource --stack-name "$STACK" --logical-resource-id 'InputBucket' --query "StackResourceDetail.PhysicalResourceId" --output text)
+BUCKET_OUT=$(aws cloudformation describe-stack-resource --stack-name "$STACK" --logical-resource-id 'ConversionTargetBucket' --query "StackResourceDetail.PhysicalResourceId" --output text)
+DYNAMO_TABLE=$(aws cloudformation describe-stack-resource --stack-name "$STACK" --logical-resource-id 'SentimentTable' --query "StackResourceDetail.PhysicalResourceId" --output text)
 
 echo "Input Bucket: $BUCKET_IN"
 echo "Ouput Bucket: $BUCKET_OUT"
-echo "Dynamo Table: $BUCKET_IN"
+echo "Dynamo Table: $DYNAMO_TABLE"
 
 ## Get Samples
 
-TEST_FILES=(~/tests/*.md)
+TEST_FILES=(tests/sample-*.md)
 
 ## Upload test samples
 
-for f in "${arr[@]}"; do
-   echo "Test file found: $f"
+for f in "${TEST_FILES[@]}"; do
+
+    FILE_NAME=$(cut -d "/" -f2- <<< "$f")
+
+    echo "Upload sample : $FILE_NAME"
+
+    aws s3 cp $f s3://"$BUCKET_IN"/"$FILE_NAME"
+
+    if [ $? -ne 0 ]; then
+        echo -e "Upload Failed" ; exit 1
+    fi
+
 done
 
 
+# Give Lambda a chance to execute or wait for last file to get to output.
 
-# echo "Clearing out resources of lambda-file-refarch stack..."
-# echo
-# echo "Cleaning up S3 buckets..." && for bucket in InputBucket ConversionTargetBucket; do
-#   echo "Clearing out ${bucket}..."
-#   BUCKET=$(aws cloudformation describe-stack-resource --stack-name lambda-file-refarch --logical-resource-id ${bucket} --query "StackResourceDetail.PhysicalResourceId" --output text)
-#   aws s3 rm s3://${BUCKET} --recursive
-#   echo
-# done
+END=$((SECONDS+30))
 
-# echo "Deleting CloudFormation stack..." && aws cloudformation delete-stack \
-# --stack-name lambda-file-refarch
+echo "Wating for execution"
 
-# echo "Clearing out CloudWatch Log Groups..." && for log_group in $(aws logs describe-log-groups --log-group-name-prefix '/aws/lambda/lambda-file-refarch-' --query "logGroups[*].logGroupName" --output text); do
-#   echo "Removing log group ${log_group}..."
-#   aws logs delete-log-group --log-group-name ${log_group}
-#   echo
-# done
+while [ $SECONDS -lt $END ]
+
+do
+
+    FILE_NAME=$(cut -d "/" -f2- <<< ${TEST_FILES[-1]})
+
+    EXISTS=$(aws s3api head-object --bucket $BUCKET_OUT --key ${FILE_NAME/md/html}) || NOT_EXIST=true
+
+    if [[ $EXISTS ]]; then
+        echo "File exists, continuing"
+        break
+    fi
+
+    echo "File doesn't exist yet... Waiting for timeout."
+    sleep 1
+
+done
+
+
+# Check for files in output bucket
+
+for f in "${TEST_FILES[@]}"; do
+
+    FILE_NAME=$(cut -d "/" -f2- <<< "$f")
+
+    echo "Checking for Output : ${FILE_NAME/md/html}"
+
+    EXISTS=(aws s3api head-object --bucket $BUCKET_OUT --key ${FILE_NAME/md/html}) || $NOT_EXIST=true
+
+    if [[ -z $EXISTS ]]; then
+        echo -e "File ${FILE_NAME/md/html} not processed" ; exit 1
+    fi
+
+    echo "${FILE_NAME/md/html} found"
+
+done
+
+# Check DynamoDB for Sentiment
+
+for f in "${TEST_FILES[@]}"; do
+
+    FILE_NAME=$(cut -d "/" -f2- <<< "$f")
+
+    echo "Checking for Sentiment : $FILE_NAME"
+
+    SENTIMENT=$(aws dynamodb get-item --table-name $DYNAMO_TABLE \
+    --projection-expression "overall_sentiment" \
+    --key "{\"id\": {\"S\": \"s3://$BUCKET_IN/$FILE_NAME\"}}")
+
+    if [[ -z $SENTIMENT  ]]; then
+      echo -e "No Sentiment" ;  exit 1
+    fi
+
+    echo $SENTIMENT
+
+done
+
+# Call Clean Up
+clean
